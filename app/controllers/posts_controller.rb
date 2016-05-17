c5353cef351db0ad1e38562d364d4095eb1650aa
codecademy <ccuser@codecademy.com>
2016-05-17 06:32:10 -0400

    Finished Project

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  
+  def index 
+    @new_post = Post.new 
+    @all_posts=Post.order(created_at: :desc).all 
+  end
+  
+  def new 
+  	@post = Post.new 
+	end
+  
+   def create 
+    @post = Post.new(post_params) 
+    if @post.save 
+      redirect_to root_path 
+    else 
+      render 'new' 
+    end 
+  end
+
+private 
+  def post_params 
+    params.require(:post).permit(:comment) 
+  end
+end
